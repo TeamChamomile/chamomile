f6b19423239782b51407212c5bfacfd81e65af72
Min Ae Cho <minae27@outlook.kr>
2021-08-13 17:19:30 +0900

    update ui

--- react-app/src/components/HeaderBarBrand.js
+++ react-app/src/components/HeaderBarBrand.js
@@ -9,12 +9,12 @@ const HeaderBarBrand = () => (
       target="_blank"
       rel="noopener noreferrer"
     >
-      <i className="fab js-logo fa-react fa-2x" aria-hidden="true" />
+      <i className="fas fa-coffee fa-2x" aria-hidden="true"/> 
     </a>
     <NavLink to="/" className="navbar-item nav-home">
-      <span className="brand-first">MY</span>
-      <span className="brand-second">SHOPPING</span>
-      <span className="brand-third">LIST</span>
+      <span className="brand-first">CHA</span>
+      <span className="brand-second">MO</span>
+      <span className="brand-third">MILE</span>
     </NavLink>
   </div>
 );
