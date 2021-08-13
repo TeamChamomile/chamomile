43dea0d76fb76bbb97ac4fa28eeebfa1f2c4a863
Min Ae Cho <minae27@outlook.kr>
2021-08-13 19:19:53 +0900

    update data,ui and about

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -3,17 +3,18 @@ import React from 'react';
 const About = () => (
   <div className="content-container">
     <div className="content-title-group not-found">
-      <h2 className="title">Product Wish List</h2>
-      <p>
-        This project was created to help represent a fundamental app written
-        with React. The shopping theme is used throughout the app.
-      </p>
+      <h2 className="title">당신의 캐모마일</h2>
+      <p> 당신은 혼자가 아니에요. </p>
+      <p>작은 마음이지만 당신을 위로하고 싶은 세명의 공순이들이 만든 웹페이지입니다. </p>
+      <p>카모마일의 꽃말을 아시나요? <b>역경 속에서의 힘</b> 이라는 뜻입니다. </p> 
+      <p>이 웹페이지가 당신의 캐모마일이 되길 바라며..! </p>
+      <p>업데이트는 천천히 꾸준히 진행됩니다 :D </p>      
       <br />
-      <h2 className="title">Resources</h2>
+      <h2 className="title">우리는!</h2>
       <ul>
         <li>
-          <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
-            Code in GitHub
+          <a href="https://github.com/TeamChamomile/chamomile">
+            건의 사항이 있으시면 언제든지 :D
           </a>
         </li>
       </ul>
