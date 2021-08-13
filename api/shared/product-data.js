841ced694be220c67f210b7d6b624fd639bce5cb
Min Ae Cho <minae27@outlook.kr>
2021-08-13 15:35:57 +0900

    add data

--- api/shared/product-data.js
+++ api/shared/product-data.js
@@ -2,20 +2,20 @@ const data = {
   products: [
     {
       id: 10,
-      name: 'Strawberries',
-      description: '16oz package of fresh organic strawberries',
+      name: '노력, 흔글',
+      description: '더 잘할 수 없어도 더 노력하는 거라면 그게 더 아름다운게 아닐까',
       quantity: '1',
     },
     {
       id: 20,
-      name: 'Sliced bread',
-      description: 'Loaf of fresh sliced wheat bread',
+      name: '양광모, 비상',
+      description: '때문에 라고 말하는 사람보다 덕분에 라고 말하는 사람이 성공한다',
       quantity: 1,
     },
     {
       id: 30,
-      name: 'Apples',
-      description: 'Bag of 7 fresh McIntosh apples',
+      name: '파올로 코엘료 마법의 순간',
+      description: '거짓말이 달아준 날개로 당신은 얼마든지 멀리 갈 수 있습니다. 그렇지만 다시 돌아오는 길은 어디에도 없어요.',
       quantity: 1,
     },
   ],
