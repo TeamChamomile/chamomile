7326078a18fd08aa1a8511af11921c7c5826cbfc
minae27 <minae27@outlook.kr>
2021-08-13 19:51:44 +0900

    Minae27/writeindetails (#6)
    * update ui
    
    * update data,ui and about
    
    * change "products" to "Quote"

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = (props) => (
     <p className="menu-label">Menu</p>
     <ul className="menu-list">
       <NavLink to="/products" activeClassName="active-link">
-        Products
+        Quote
       </NavLink>
       <NavLink to="/about" activeClassName="active-link">
         About
